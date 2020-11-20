f0c09d5a991e2a5456c4b3e2337acf1bb4d03478
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2020-11-20 11:39:29 +0100

    Create plotHiggs.py

---
+++ plotHiggs.py
@@ -0,0 +1,222 @@
+# plot configuration
+
+
+
+# groupPlot = {}
+# 
+# Groups of samples to improve the plots.
+# If not defined, normal plots is used
+#
+
+
+
+
+#groupPlot['Higgs']  = {  
+#                  'nameHR' : 'Higgs',
+#                  'isSignal' : 1,
+#                  'color': 632, # kRed 
+#		  #'samples'  : ['H_htt', 'H_hww', 'ZH_hww', 'ggZH_hww', 'WH_hww', 'qqH_hww', 'ggH_hww','bbH_hww','ttH_hww','ZH_htt', 'ggZH_htt', 'WH_htt', 'qqH_htt', 'ggH_htt','bbH_htt','ttH_htt' ]
+#		  'samples'  : ['H_htt', 'H_hww', 'ZH_hww', 'ggZH_hww', 'WH_hww', 'qqH_hww', 'ggH_hww','bbH_hww','ttH_hww', 'qqH_htt', 'ggH_htt' ]
+#              }
+              
+              
+groupPlot['ZH_hww'] = {
+                  'nameHR'    : 'ZH',
+                  'color'     :  418, #kGreen
+                  'isSignal'  :  0,
+                  'isData'    :  0,
+                  'scale'     :  1
+}
+
+groupPlot['ggZH_hww'] = {
+                  'nameHR'    : 'ggZH',
+                  'color'     :  851, #kAzure-9
+                  'isSignal'  :  0,
+                  'isData'    :  0,
+                  'scale'     :  1
+}
+
+
+groupPlot['WH_hww'] = {
+                  'nameHR'    : 'WH',
+                  'color'     :  810, #kOrange
+                  'isSignal'  :  0,
+                  'isData'    :  0,
+                  'scale'     :  1
+}
+
+groupPlot['qqH_hww'] = {         
+                  'nameHR'    : 'qqH (VBF)',   #VBF
+                  'color'     :  400, #kYellow
+                  'isSignal'  :  0,
+                  'isData'    :  0,
+                  'scale'     :  1
+}
+
+groupPlot['ggH_hww'] = {
+                  'nameHR'    : 'ggH',
+                  'color'     :  632, #kRed
+                  'isSignal'  :  0,
+                  'isData'    :  0,
+                  'scale'     :  1
+}
+
+
+
+
+
+#plot = {}
+
+# keys here must match keys in samples.py    
+#                    
+
+# Higgs -> tautau
+
+#plot['ZH_htt'] = {
+#                  'nameHR' : 'ZHtt',
+#                  'color': 632+3, # kRed+3 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,    
+#                  'scale'    : 1    #
+#                  }
+#
+#plot['bbH_htt'] = {
+#                  'nameHR' : 'bbHtt',
+#                  'color': 632-1, # kRed-1 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,
+#                  'scale'    : 1    #
+#                  }
+#
+#plot['ttH_htt'] = {
+#                  'nameHR' : 'bbHtt',
+#                  'color': 632-2, # kRed-1 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,
+#                  'scale'    : 1    #
+#                  }
+#
+#
+#plot['ggZH_htt'] = {
+#                  'nameHR' : 'ggZHtt',
+#                  'color': 632+4, # kRed+4
+#                  'isSignal' : 1,
+#                  'isData'   : 0,    
+#                  'scale'    : 1    #
+#                  }
+#
+#plot['WH_htt'] = {
+#                  'nameHR' : 'WHtt',
+#                  'color': 632+2, # kRed+2 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,    
+#                  'scale'    : 1    #
+#                  }
+#
+#
+#plot['qqH_htt'] = {
+#                  'nameHR' : 'qqHtt',
+#                  'color': 632+1, # kRed+1 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,    
+#                  'scale'    : 1    #
+#                  }
+#
+#
+#plot['ggH_htt'] = {
+#                  'nameHR' : 'ggHtt',
+#                  'color': 632, # kRed 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,    
+#                  'scale'    : 1    #
+#                  }
+#
+
+
+# HWW 
+
+#plot['H_hww'] = {
+#                  'nameHR' : 'Hww',
+#                  'color': 632, # kRed 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,    
+#                  'scale'    : 1    #
+#                  }
+
+plot['ZH_hww'] = {
+                  'nameHR' : 'ZH',
+                  'color': 632+3, # kRed+3 
+                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+plot['ggZH_hww'] = {
+                  'nameHR' : 'ggZH',
+                  'color': 632+4, # kRed+4
+                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+plot['WH_hww'] = {
+                  'nameHR' : 'WH',
+                  'color': 632+2, # kRed+2 
+                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+
+plot['qqH_hww'] = {
+                  'nameHR' : 'qqH',
+                  'color': 632+1, # kRed+1 
+                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+
+plot['ggH_hww'] = {
+                  'nameHR' : 'ggH',
+                  'color': 632, # kRed 
+                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+#plot['bbH_hww'] = {
+#                  'nameHR' : 'bbH',
+#                  'color': 632+5, # kRed+5 
+#                  'isSignal' : 1,
+#                  'isData'   : 0,
+#                  'scale'    : 1    #
+#                  }
+
+#plot['ttH_hww'] = {
+#                  'nameHR' : 'ttH',
+#                  'color': 632+6, # kRed+6
+#                  'isSignal' : 1,
+#                  'isData'   : 0,
+#                  'scale'    : 1    #
+#                  }
+#
+
+# data
+
+plot['DATA']  = { 
+                  'nameHR' : 'Data',
+                  'color': 1 ,  
+                  'isSignal' : 0,
+                  'isData'   : 1 ,
+                  'isBlind'  : 1 #Is blind == 1 pinta solo MC, 0 pinta tambien datos.
+              }
+
+
+
+
+# additional options
+
+legend['lumi'] = 'L = 35.9/fb'
+
+legend['sqrt'] = '#sqrt{s} = 13 TeV'
