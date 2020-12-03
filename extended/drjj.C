3932c547100e4c2e090682ec5a68a8650066620e
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2020-12-03 15:55:29 +0100

    Create drjj.C

---
+++ extended/drjj.C
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+float detaljmin(float jet1_eta,
+		float jet2_eta,
+		float jet1_phi,
+		float jet2_hi){
+    
+  return TMath::sqrt(jet1_eta * jet2_eta + jet1_phi * jet2_phi)   
+    
+}  
