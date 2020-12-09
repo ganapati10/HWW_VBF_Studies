7724c7f63cba770ab7fdfaa59e095591748c2035
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2020-12-09 21:52:14 +0100

    Update Plot_Sig_Bkg.py

--- Plot_Sig_Bkg.py
+++ Plot_Sig_Bkg.py
@@ -91,11 +91,91 @@ class Plot_Sig_Bkg:
     
     list_canvas = {}
     
+    #Loop under variables
     for variableName, variable in self._variables.iteritems():
       
-      tcanvas = ROOT.TCanvas("Signal vs Background_" + variableName, "Signal vs Background_" + variableName, 800, 600)
+      tcanvas = ROOT.TCanvas("Signal vs Background_" + variableName, "Signal vs Background_" + variableName, 800, 600) #Iniciate the canvas, one for each variable.
       
-      list_canvas [counter] = tcanvas
+      list_canvas [counter] = tcanvas   #list the canvas
+      
+      counter = counter + 1
+      
+      #For each cut in cuts.py
+      for cutName in self._cuts:
+        print "cut_", cutName
+        
+        if 'cuts' in variable and cutName not in variable['cuts']:
+          continue
+
+        if type(fileIn) is not dict and not fileIn.GetDirectory(cutName+"/"+variableName):
+          continue
+          
+        print "variableName =", variableName
+            
+        # Define variables for store bin content of histograms
+        sig  = 0
+        bkg  = 0
+        data = 0
+
+        
+        for samplesName, plotdef in plot:
+          
+          if 'samples' in variable and sampleName not in variable['samples']:
+            continue
+
+          shapeName = cutName+"/"+variableName+'/histo_' + sampleName
+          
+          if type(fileIn) is dict:
+            histo = fileIn[sampleName].Get(shapeName)
+          else:
+            histo = fileIn.Get(shapeName)
+          print ' --> ', histo
+          print 'new_histo_' + sampleName + '_' + cutName + '_' + variableName
+          histogram = histo.Clone('new_histo_' + sampleName + '_' + cutName + '_' + variableName)
+          
+          if plotdef['isSignal'] == 1 :
+            
+            sig = sig + histogram.GetEntries()
+            
+          elif plotdef['isSignal'] == 0 :
+            
+            bkg = bkg + histogram.GetEntries()
+            
+          elif plotdef['isData'] == 1 :
+            
+            data = data + histogram.GetEntries()
+          
+        cuts = {}
+        
+        if os.path.exists(opt.self._cuts) :
+          handle = open(opt.self._cuts,'r')
+          exec(handle)
+          handle.close()      
+        
+        for cut_k, cut_v in cuts.items():
+          if cut_k == cutName:
+            txt = cut_v[0].split("&&")
+          for cuts_ind in txt:
+            cut_ind = cuts_ind.split('>')
+            
+            if len(var) < 2:
+              cut_ind = cuts_ind.split('<')
+              
+              if len(var) < 2:
+                    cut_ind = cuts_ind.split('=')
+            
+            if cut_ind[0].strip() == cut_k.split("_")[0]:
+                
+                num = cut_ind[1]
+                print(num) ##Cambiar esto, arreglar
+
+
+         
+        
+        
+            
+          
+          
       
       
       
