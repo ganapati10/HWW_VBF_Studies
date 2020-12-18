5bfd6516b89c962df04cfcd4af42d03039e6bd7f
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2020-12-18 08:37:12 +0100

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -9,11 +9,6 @@
 #These scripts are going to be runned:
 mkShapesMulti.py --pycfg=configuration.py --inputDir=/gpfs/projects/cms/data/LatinosSkims/nanoAOD/ --treeName=Events
 
-mkShapesMulti.py --doHadd=1 --doNotCleanup --nThreads=8
-
-mkPlot.py --pycfg=configuration.py --inputFile=rootFile/plots_WW_2016.root --minLogC=0.01 --minLogCratio=0.01 --maxLogC=1000 --maxLogCratio=1000 --showIntegralLegend=1
-
-
 
 
 
