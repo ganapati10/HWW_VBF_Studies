99713d04efaae7f74e049aa4e0331fab88f52a7b
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2021-02-08 14:38:03 +0100

    Update plot.py

--- plot.py
+++ plot.py
@@ -71,15 +71,28 @@ groupPlot['VgS']  = {
 
 
 
-groupPlot['Higgs']  = {  
+groupPlot['Higgs']  = {
                   'nameHR' : 'Higgs',
-                  'isSignal' : 1,
-                  'color': 632, # kRed 
-		  #'samples'  : ['H_htt', 'H_hww', 'ZH_hww', 'ggZH_hww', 'WH_hww', 'qqH_hww', 'ggH_hww','bbH_hww','ttH_hww','ZH_htt', 'ggZH_htt', 'WH_htt', 'qqH_htt', 'ggH_htt','bbH_htt','ttH_htt' ]
-		  'samples'  : ['H_htt', 'H_hww', 'ZH_hww', 'ggZH_hww', 'WH_hww', 'qqH_hww', 'ggH_hww','bbH_hww','ttH_hww', 'qqH_htt', 'ggH_htt' ]
+                  'isSignal' : 0,
+                  'color': 920, # kRed                                                                                                                                                                      
+                  #'samples'  : ['H_htt', 'H_hww', 'ZH_hww', 'ggZH_hww', 'WH_hww', 'qqH_hww', 'ggH_hww','bbH_hww','ttH_hww','ZH_htt', 'ggZH_htt', 'WH_htt', 'qqH_htt', 'ggH_htt','bbH_htt','ttH_htt' ]      
+                  #'samples'  : ['H_htt', 'H_hww', 'ZH_hww', 'ggZH_hww', 'WH_hww', 'ggH_hww','bbH_hww','ttH_hww', 'qqH_htt', 'ggH_htt' ] with ggH                                                           
+                  'samples'  : ['H_htt', 'H_hww', 'ZH_hww', 'ggZH_hww', 'WH_hww','bbH_hww','ttH_hww', 'qqH_htt', 'ggH_htt' ]
               }
 
+groupPlot['VBF']  = {
+                  'nameHR' : "Higgs VBF",
+                  'isSignal' : 1,
+                  'color'    : 600,   # kBlue                                                                                                                                                              
+                  'samples'  : ['qqH_hww']
+              }
 
+groupPlot['ggF']  = {
+                  'nameHR' : "Higgs ggF",
+                  'isSignal' : 1,
+                  'color'    : 632,   # kBlue                                                                                                                                                              
+                  'samples'  : ['ggH_hww']
+              }
 
 
 
@@ -280,45 +293,47 @@ groupPlot['Higgs']  = {
 
 plot['ZH_hww'] = {
                   'nameHR' : 'ZH',
-                  'color': 632+3, # kRed+3 
-                  'isSignal' : 1,
-                  'isData'   : 0,    
-                  'scale'    : 1    #
+                  'color': 920+3, # kRed+3                                                                                                                                                                  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1    #                                                                                                                                                                       
                   }
 
 plot['ggZH_hww'] = {
                   'nameHR' : 'ggZH',
-                  'color': 632+4, # kRed+4
-                  'isSignal' : 1,
-                  'isData'   : 0,    
-                  'scale'    : 1    #
+                  'color': 920+4, # kRed+4                                                                                                                                                                  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1    #                                                                                                                                                                       
                   }
 
 plot['WH_hww'] = {
                   'nameHR' : 'WH',
-                  'color': 632+2, # kRed+2 
-                  'isSignal' : 1,
-                  'isData'   : 0,    
-                  'scale'    : 1    #
+                  'color': 920+2, # kRed+2                                                                                                                                                                  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1    #                                                                                                                                                                       
                   }
 
 
 plot['qqH_hww'] = {
                   'nameHR' : 'qqH',
-                  'color': 632+1, # kRed+1 
+                  'color': 632+1, # kRed+1                                                                                                                                                                  
                   'isSignal' : 1,
-                  'isData'   : 0,    
-                  'scale'    : 1    #
+                  'isData'   : 0,
+                  'scale'    : 1    #                                                                                                                                                                       
                   }
 
 
 plot['ggH_hww'] = {
                   'nameHR' : 'ggH',
-                  'color': 632, # kRed 
-                  'isSignal' : 1,
-                  'isData'   : 0,    
-                  'scale'    : 1    #
+                  'color': 632, # kRed                                                                                                                                                                      
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1    #                                                                                                                                                                       
                   }
+
+
 
 #plot['bbH_hww'] = {
 #                  'nameHR' : 'bbH',
