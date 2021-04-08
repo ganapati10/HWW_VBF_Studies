142ad01019b5fde7112113024f5d0597423e5cc2
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2021-04-08 11:16:43 +0200

    Update configuration_fit.py

--- Full2018_v7/configuration_fit.py
+++ Full2018_v7/configuration_fit.py
@@ -2,7 +2,7 @@
 
 treeName = 'Events'
 
-tag = 'WW_2016'
+tag = 'WW_2018'
 
 # used by mkShape to define output directory for root files                                                                                                                                                
 outputDir = 'rootFile'
@@ -14,20 +14,20 @@ aliasesFile = 'aliases.py'
 variablesFile = 'variables_fit.py'
 
 # file with list of cuts                                                                                                                                                                                   
-cutsFile = 'cuts_fit.py'
+cutsFile = 'cuts.py'
 
 # file with list of samples                                                                                                                                                                                
 samplesFile = 'samples.py'
 
 # file with plot configuration                                                                                                                                                                             
-plotFile = 'plot_fit.py'
+plotFile = 'plot.py'
 
 # luminosity to normalize to (in 1/fb)                                                                                                                                                                     
-lumi = 35.867
+lumi = 59.74
 
 # used by mkPlot to define output directory for plots                                                                                                                                                      
 # different from "outputDir" to do things more tidy                                                                                                                                                        
-outputDirPlots = 'plotWW_2016'
+outputDirPlots = 'plotWW_2018'
 
 # used by mkDatacards to define output directory for datacards                                                                                                                                             
 outputDirDatacard = 'datacards'
