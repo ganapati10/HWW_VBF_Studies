fbd221f207d673dc77980bbe346b8e462932a897
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2021-04-08 11:08:53 +0200

    Update configuration.py

--- Full2018_v7/configuration.py
+++ Full2018_v7/configuration.py
@@ -2,7 +2,7 @@
 
 treeName = 'Events'
 
-tag = 'WW_2016_select'
+tag = 'WW_2018_select'
 
 # used by mkShape to define output directory for root files
 outputDir = 'rootFile'
@@ -14,20 +14,20 @@ aliasesFile = 'aliases.py'
 variablesFile = 'variables.py'
 
 # file with list of cuts
-cutsFile = 'cuts2.py' 
+cutsFile = 'cuts.py' 
 
 # file with list of samples
-samplesFile = 'samples_fit2.py' 
+samplesFile = 'samples.py' 
 
 # file with plot configuration
-plotFile = 'plot_fit.py' 
+plotFile = 'plot.py' 
 
 # luminosity to normalize to (in 1/fb)
-lumi = 41.53
+lumi = 59.74
 
 # used by mkPlot to define output directory for plots
 # different from "outputDir" to do things more tidy
-outputDirPlots = 'plotWW_2016'
+outputDirPlots = 'plotWW_2018'
 
 # used by mkDatacards to define output directory for datacards
 outputDirDatacard = 'datacards'
