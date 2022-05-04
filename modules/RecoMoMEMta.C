609e99ca79b6c9347ccd16c85e9da79db27bbfdc
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2022-05-04 13:00:43 +0200

    Update RecoMoMEMta.C

--- modules/RecoMoMEMta.C
+++ modules/RecoMoMEMta.C
@@ -28,19 +28,39 @@ void normalizeInput(LorentzVector& p4) {
 }
 
 
-float recoMoMEMta(higgspx, higgspy, higgspz, higgse, jet1pt, jet2pt, jet1phi, jet2phi, jet1eta, jet2eta, path){
+float recoMoMEMta(float nCleanJet, float nLepton, float PuppiMet_pt, float PuppiMet_phi, float Lepton_pt0, float Lepton_pt1, float Lepton_phi0, float Lepton_phi1, float Lepton_eta0, float Lepton_eta1,flo\
+at CleanJet_pt0, float CleanJet_pt1, float CleanJet_phi0, float CleanJet_phi1, float CleanJet_eta0, float CleanJet_eta1, float Lepton_pdg0, float Lepton_pdg1){
   
   logging::set_level(logging::level::off);
   
   //Initializing 4-vectors
+  TLorentzVector L1(0.,0.,0.,0.);
+  TLorentzVector L2(0.,0.,0.,0.);
+  TLorentzVector LL(0.,0.,0.,0.);
+  TLorentzVector NuNu(0.,0.,0.,0.);
+  TLorentzVector Higgs(0.,0.,0.,0.);
   TLorentzVector J1(0.,0.,0.,0.);
   TLorentzVector J2(0.,0.,0.,0.);
   
-  J1.SetPtEtaPhiM(jet1pt, jet1eta, jet1phi, 0.0);
-  J2.SetPtEtaPhiM(jet2pt, jet2eta, jet2phi, 0.0);
+  L1.SetPtEtaPhiM(Lepton_pt0, Lepton_eta0, Lepton_phi0, 0.0);
+  L2.SetPtEtaPhiM(Lepton_pt1, Lepton_eta1, Lepton_phi1, 0.0);
+
+  J1.SetPtEtaPhiM(CleanJet_pt0, CleanJet_eta0, CleanJet_phi0, 0.0);
+  J2.SetPtEtaPhiM(CleanJet_pt1, CleanJet_eta1, CleanJet_phi1, 0.0);
+
+  LL = L1 + L2;
+
+  double nunu_px = PuppiMet_pt*cos(PuppiMet_phi);
+  double nunu_py = PuppiMet_pt*sin(PuppiMet_phi);
+  double nunu_pz = LL.Pz();
+  double nunu_m = 30.0; //Why 30? --> https://indico.cern.ch/event/850505/contributions/3593915/                                                                                                          
+
+  double nunu_e = sqrt(nunu_px*nunu_px + nunu_py*nunu_py + nunu_pz*nunu_pz + nunu_m*nunu_m);
+  NuNu.SetPxPyPzE(nunu_px, nunu_py, nunu_pz, nunu_e);
+  Higgs = LL + NuNu;
   
-  momemta::Particle higgs { "higgs", LorentzVector(higgspx, higgspy, higgspz, higgse), 25 }; // Higgs                                                                                                    
-  momemta::Particle Z { "Z", LorentzVector(higgspx, higgspy, higgspz, higgse), 23 }; // Z, same 4 vector as Higgs                                                                             
+  momemta::Particle higgs { "higgs", LorentzVector(Higgs.Px(), Higgs.Py(), Higgs.Pz(), Higgs.E()), 25 }; // Higgs                                                                                                    
+  momemta::Particle Z { "Z", LorentzVector(Higgs.Px(), Higgs.Py(), Higgs.Pz(), Higgs.E()), 23 }; // Z, same 4 vector as Higgs                                                                             
   momemta::Particle jet1 { "jet1", LorentzVector(J1.Px(), J1.Py(), J1.Pz(), J1.E()), 1 };
   momemta::Particle jet2 { "jet2", LorentzVector(J2.Px(), J2.Py(), J2.Pz(), J2.E()), -1 };
 
